4d7ea07ccbf0da3691536855b7f9829f593dd313
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-04-28 22:35:59 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryImages = galleryItems
+  .map((image) => {
+    return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>
+</li>
+  `;
+  })
+  .join('');
+
+gallery.innerHTML = galleryImages;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  capttion: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
